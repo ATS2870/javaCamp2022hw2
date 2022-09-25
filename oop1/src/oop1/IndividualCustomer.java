e17dbb4a55b5f7f3f5043ed23a05248d71433cfe
AHMET TARIK SERİN <ahmettarikserin@gmail.com>
2022-09-25 12:53:07 +0300

    secondweek

---
+++ oop1/src/oop1/IndividualCustomer.java
@@ -0,0 +1,20 @@
+package oop1;
+
+public class IndividualCustomer extends Customer{
+	private String firstName;
+	private String lastname;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+            
+}
---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,16 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.Topla(3, 4);
+		int sonuc2 = dortIslem.Carp(3, 4);
+		int sonuc3 = dortIslem.Cıkar(3, 4);
+		System.out.println(sonuc);
+		System.out.println(sonuc2);
+		System.out.println(sonuc3);
+
+	}
+
+}
